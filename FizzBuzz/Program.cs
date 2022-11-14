63bd8ac591056a63d9e97489f294f7b78c7d6729
Mike Cowell <spike@spikeandbetty.com>
2022-11-14 16:03:57 -0500

    init commit

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    public class Program
+    {
+        static string FizzBuzz(int x)
+        {
+            return (x % 5 == 0 && x % 3 == 0) ? "fizbuzz" : (x % 5 == 0) ? "buzz" : (x % 3 == 0) ? "fizz" : null;
+
+            //if(x % 5 == 0 && x%3 == 0)
+            //{
+            //    return "fizbuzz";
+            //}
+            //else if(x % 5 == 0)
+            //{
+            //    return "buzz";
+            //}
+            //else if(x % 3 == 0)
+            //{
+            //    return "fizz";
+            //}
+            //else
+            //{
+            //    return null;
+            
+        }
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine(FizzBuzz(15));
+            Console.WriteLine(FizzBuzz(10));
+            Console.WriteLine(FizzBuzz(6));
+            Console.WriteLine(FizzBuzz(7));
+            
+
+        }
+    }
+}
